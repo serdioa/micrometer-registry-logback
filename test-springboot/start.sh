0679b04892da58cb7c64feac649eb45eb2120440
serdioa <serdioa@t-online.de>
2020-03-28 15:04:59 +0100

    Added skeleton implementation of a Spring-Boot test application

---
+++ test-springboot/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run from Maven
+mvn spring-boot:run -Dspring.config.location=file:src/config/test-springboot.properties "$@"
+
+# Package as runnable JAR and start
+# mvn package spring-boot:repackage
+# java -Dspring.config.location=file:src/config/test-springboot.properties -jar target/test-springboot-1.0-SNAPSHOT.jar "$@"

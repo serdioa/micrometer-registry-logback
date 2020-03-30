9307e1157b8fc05da73d16a4026d48cbfcd4db3b
serdioa <serdioa@t-online.de>
2020-03-30 19:25:50 +0200

    Add configuration to disable some metrics

--- test-springboot/src/main/java/de/serdioa/micrometer/spring/test/MetricsPublisherBean.java
+++ test-springboot/src/main/java/de/serdioa/micrometer/spring/test/MetricsPublisherBean.java
@@ -1,6 +1,9 @@
 package de.serdioa.micrometer.spring.test;
 
 import de.serdioa.micrometer.test.CompositeMetricsPublisher;
+import de.serdioa.micrometer.test.CounterPublisher;
+import de.serdioa.micrometer.test.GaugePublisher;
+import de.serdioa.micrometer.test.TimeGaugePublisher;
 import de.serdioa.micrometer.test.TimerPublisher;
 import io.micrometer.core.instrument.MeterRegistry;
 import javax.annotation.PostConstruct;
@@ -22,14 +25,14 @@ public class MetricsPublisherBean {
     public void init() {
         this.publisher = new CompositeMetricsPublisher();
 
-//        this.publisher.add(new CounterPublisher(this.registry));
+        this.publisher.add(new CounterPublisher(this.registry));
 //        this.publisher.add(new DistributionSummaryPublisher(this.registry));
 //        this.publisher.add(new FunctionCounterPublisher(this.registry));
 //        this.publisher.add(new FunctionTimerPublisher(this.registry));
 //        this.publisher.add(new JvmMetricsPublisher(this.registry));
-//        this.publisher.add(new GaugePublisher(this.registry));
+        this.publisher.add(new GaugePublisher(this.registry));
 //        this.publisher.add(new LongTaskTimerPublisher(this.registry));
-//        this.publisher.add(new TimeGaugePublisher(this.registry));
+        this.publisher.add(new TimeGaugePublisher(this.registry));
         this.publisher.add(new TimerPublisher(this.registry));
 
         this.publisher.start();

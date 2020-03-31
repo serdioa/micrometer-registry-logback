00da3b925157c2ddecbbfaa2d2670554dd76f89c
serdioa <serdioa@t-online.de>
2020-03-31 17:35:14 +0200

    Add configuration property to disable MetricsConsumer

--- test-springboot/src/main/java/de/serdioa/micrometer/spring/test/MetricsConsumer.java
+++ test-springboot/src/main/java/de/serdioa/micrometer/spring/test/MetricsConsumer.java
@@ -3,14 +3,19 @@ package de.serdioa.micrometer.spring.test;
 import de.serdioa.micrometer.test.MetricsFormatter;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@ConditionalOnProperty(prefix = "de.serdioa.micrometer.spring.test.MetricsConsumer", name = "enabled",
+        havingValue = "true", matchIfMissing = true)
 public class MetricsConsumer {
+
     @Autowired
     private MeterRegistry registry;
+
 
     @Scheduled(fixedRate = 5000)
     public void consumeMetrics() {

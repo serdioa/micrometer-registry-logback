83a43529c906b0d3daf7fe73d45cae5230f396a5
serdioa <serdioa@t-online.de>
2021-05-18 09:43:49 +0200

    Added test property source

--- test-spring/src/main/java/de/serdioa/micrometer/spring/test/ApplicationConfiguration.java
+++ test-spring/src/main/java/de/serdioa/micrometer/spring/test/ApplicationConfiguration.java
@@ -2,9 +2,11 @@ package de.serdioa.micrometer.spring.test;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 
 @Configuration
+@PropertySource("classpath:/de/serdioa/micrometer/spring/test/application.properties")
 public class ApplicationConfiguration {
     @Bean
     public TestBean testBean() {
